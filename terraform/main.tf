98b88a11e87fb5e844f8343e4a94bcb65a676bed
Eric Noh <sehyun.noh83@gmail.com>
2026-02-15 17:15:49 -0500

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,10 @@ terraform {
     }
   }
 
+  # 🏠 Backend: 쉘 스크립트가 만든 '관리용' 저장소를 바라봅니다.
   backend "azurerm" {
-    resource_group_name  = "pilot-rg"
-    storage_account_name = "pilotstorage123"
+    resource_group_name  = "pilot-mgmt-rg"
+    storage_account_name = "pilotbackendstorage123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
@@ -18,34 +19,37 @@ provider "azurerm" {
   features {}
 }
 
+# --- Variables ---
 variable "location" {
   default = "Canada Central"
 }
 
-variable "resource_group" {
-  default = "pilot-rg"
+variable "service_rg_name" {
+  default = "pilot-service-rg" # 서비스용 RG 이름 분리
 }
 
-variable "storage_account_name" {
-  default = "pilotstorage123"
+variable "service_storage_name" {
+  default = "pilotappstorage123" # 서비스용 Storage 이름 분리
 }
 
-# Resource Group
+# --- Resources (Service Layer) ---
+
+# 1. 서비스용 Resource Group (이제 테라포름이 직접 생성/관리함)
 resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group
+  name     = var.service_rg_name
   location = var.location
 }
 
-# Storage Account
+# 2. 서비스용 Storage Account
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
+  name                     = var.service_storage_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# Blob Containers
+# 3. Blob Containers
 locals {
   containers = ["inbound", "archive", "out-united", "out-elf", "out-economics"]
 }
@@ -57,16 +61,16 @@ resource "azurerm_storage_container" "containers" {
   container_access_type = "private"
 }
 
-# Function App Service Plan
+# 4. Function App Service Plan
 resource "azurerm_service_plan" "plan" {
   name                = "pilot-func-plan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "Y1" # Consumption Plan
+  sku_name            = "Y1"
 }
 
-# Function App (Managed Identity)
+# 5. Function App
 resource "azurerm_linux_function_app" "func" {
   name                       = "pilot-blob-func"
   location                   = azurerm_resource_group.rg.location
@@ -86,12 +90,11 @@ resource "azurerm_linux_function_app" "func" {
   }
 
   app_settings = {
-    # Storage URL는 Managed Identity 사용 시 읽는 코드에서 reference
     STORAGE_ACCOUNT_URL = "https://${azurerm_storage_account.storage.name}.blob.core.windows.net"
   }
 }
 
-# Role Assignment: Function -> Storage
+# 6. Role Assignment
 resource "azurerm_role_assignment" "func_storage_access" {
   scope                = azurerm_storage_account.storage.id
   role_definition_name = "Storage Blob Data Contributor"
