c84e57dfda74a2b9756278df7a6c871a9e6589bb
Eric Noh <sehyun.noh83@gmail.com>
2026-02-15 22:56:50 -0500

    change plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,7 +82,7 @@ resource "azurerm_service_plan" "plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "B1"
 
   depends_on = [
     azurerm_storage_account.storage
