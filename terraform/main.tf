a710df067c5217b2398945fd00a53fb1b6703d15
Eric Noh <sehyun.noh83@gmail.com>
2026-02-15 23:19:25 -0500

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -78,11 +78,11 @@ resource "azurerm_storage_container" "containers" {
 # 4. App Service Plan (Y1)
 # -----------------------------
 resource "azurerm_service_plan" "plan" {
-  name                = "pilot-func-plan-v2"
+  name                = "pilot-func-plan-v3"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "B1"
 
   depends_on = [
     azurerm_storage_account.storage
