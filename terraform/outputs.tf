682dae9b4ca41297829e553b63fc671cbe1013b4
Eric Noh <sehyun.noh83@gmail.com>
2026-02-24 17:09:04 -0500

    update

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,7 @@ output "resource_group_name" {
 output "function_app_name" {
   value = azurerm_function_app_flex_consumption.func.name
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
